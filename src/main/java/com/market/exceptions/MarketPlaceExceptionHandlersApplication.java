d6905bd0f09a602d3f390e190cf86b0387a58eaf
VandersonAssis <assis.vanderson@gmail.com>
2019-10-10 20:13:38 -0300

    - Initial commit

---
+++ src/main/java/com/market/exceptions/MarketPlaceExceptionHandlersApplication.java
@@ -0,0 +1,13 @@
+package com.market.exceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketPlaceExceptionHandlersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketPlaceExceptionHandlersApplication.class, args);
+	}
+
+}
